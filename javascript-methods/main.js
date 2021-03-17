999b082064f04850aaa3dc1e48e37b235b8a4f17
Mikaela Duguil <kimmieduggz@gmail.com>
2021-03-17 10:52:45 -0700

    changed let to var

--- javascript-methods/main.js
+++ javascript-methods/main.js
@@ -1,26 +1,25 @@
-/* eslint-disable prefer-const */
-let num1 = 12;
-let num2 = 49;
-let num3 = 24;
+var num1 = 12;
+var num2 = 49;
+var num3 = 24;
 
-let maximumValue = Math.max(num1, num2, num3);
+var maximumValue = Math.max(num1, num2, num3);
 
 console.log(maximumValue);
 
-let heroes = ['Wonder Woman', 'Superman', 'Jean Grey', 'Jane Foster'];
+var heroes = ['Wonder Woman', 'Superman', 'Jean Grey', 'Jane Foster'];
 
-let randomNumnber = Math.random();
+var randomNumnber = Math.random();
 randomNumnber *= heroes.length;
 
-let randomIndex = Math.floor(randomNumnber);
+var randomIndex = Math.floor(randomNumnber);
 
 console.log('randomIndex: ', randomIndex);
 
-let randomHero = heroes[randomIndex];
+var randomHero = heroes[randomIndex];
 
 console.log('randomHero: ', randomHero);
 
-let library = [
+var library = [
   {
     author: 'Leo Tolstoy',
     title: 'Anna Karenina'
@@ -35,20 +34,20 @@ let library = [
   }
 ];
 
-let lastBook = library.pop();
+var lastBook = library.pop();
 
 console.log('lastBook: ', lastBook);
 
-let firstBook = library.shift();
+var firstBook = library.shift();
 
 console.log('firstBook: ', firstBook);
 
-let js = {
+var js = {
   title: 'JavaScript for Impatient Programmers',
   author: 'Dr. Axel Rauschmayer'
 };
 
-let css = {
+var css = {
   title: 'CSS Secrets',
   author: 'Lea Verou'
 };
@@ -60,12 +59,12 @@ library.splice(1, 1);
 
 console.log('library: ', library);
 
-let fullName = 'Mikaela ' + 'Duguil';
-let firstAndLastName = fullName.split(' ');
+var fullName = 'Mikaela ' + 'Duguil';
+var firstAndLastName = fullName.split(' ');
 console.log('firstAndLastName: ', firstAndLastName);
 
-let firstName = firstAndLastName[0];
+var firstName = firstAndLastName[0];
 
-let sayMyName = firstName.toUpperCase();
+var sayMyName = firstName.toUpperCase();
 
 console.log('sayMyName: ', sayMyName);

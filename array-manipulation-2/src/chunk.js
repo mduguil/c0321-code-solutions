c0f434dfcfc4c46386e5bd80d70307d73bb1dcdf
Mikaela Duguil <kimmieduggz@gmail.com>
2021-03-23 16:37:40 -0700

    working on chunk

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -2,5 +2,13 @@
 function chunk(array, size) {
   if (array.length === 0) return [];
   var chunky = [];
+  for (var i = 0; i < array.length; i++) {
+    var babyChunk = [];
+    babyChunk.push(array[i]);
+    if (babyChunk.length === size) {
+      chunk(array[i]);
+    }
+    chunky.push(babyChunk);
+  }
   return chunky;
 }

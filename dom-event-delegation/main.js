e3dc7ecaa59571a90f1f8c2295085654b0e7afee
Mikaela Duguil <kimmieduggz@gmail.com>
2021-03-25 13:18:16 -0700

    .closest

---
+++ dom-event-delegation/main.js
@@ -0,0 +1,11 @@
+var $taskList = document.querySelector('.task-list');
+
+$taskList.addEventListener('click', function (event) {
+  console.log('event.target: ', event.target);
+  console.log('event.target.tagName: ', event.target.tagName);
+  if (event.target.matches('button')) {
+    var closestElem = event.target.closest('.task-list-item');
+    console.log('closest .task-list-item: ', closestElem);
+    closestElem.remove();
+  }
+});

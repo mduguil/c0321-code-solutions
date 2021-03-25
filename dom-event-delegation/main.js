bca0693b37e0c476b59725f52f0717176271403b
Mikaela Duguil <kimmieduggz@gmail.com>
2021-03-25 13:26:01 -0700

    changed var to document

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -1,6 +1,5 @@
-var $taskList = document.querySelector('.task-list');
 
-$taskList.addEventListener('click', function (event) {
+document.addEventListener('click', function (event) {
   console.log('event.target: ', event.target);
   console.log('event.target.tagName: ', event.target.tagName);
   if (event.target.matches('button')) {

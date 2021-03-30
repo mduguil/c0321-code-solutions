e90c825b1438c202f1d183eab72bc3fcf5e2344e
Mikaela Duguil <kimmieduggz@gmail.com>
2021-03-30 11:20:58 -0700

    deleted redundand code

--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,6 +1,5 @@
 /* exported dropRight */
 function dropRight(array, count) {
-  if (array.length === 0) return [];
   var newArr = [];
   for (var i = 0; i < array.length - count; i++) {
     newArr.push(array[i]);
--- array-manipulation-2/src/drop.js
+++ array-manipulation-2/src/drop.js
@@ -1,6 +1,5 @@
 /* exported drop */
 function drop(array, count) {
-  if (array.length === 0) return [];
   var newArr = [];
   for (var i = count; i < array.length; i++) {
     newArr.push(array[i]);

799470d3fce02f2e8ab9c911564b2843d14022f5
Mikaela Duguil <kimmieduggz@gmail.com>
2021-04-06 21:08:45 -0700

    reversewords

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,10 @@
 /* exported reverseWords */
+function reverseWords(string) {
+  var wordsArr = string.split(' ');
+  var reversedWordsArr = [];
+  for (var i = 0; i < wordsArr.length; i++) {
+    var reversedWord = wordsArr[i].split('').reverse().join('');
+    reversedWordsArr.push(reversedWord);
+  }
+  return reversedWordsArr.join(' ');
+}

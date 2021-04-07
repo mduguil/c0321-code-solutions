0bea9ec60e0322b2570f55220c3eef620c90d02f
Mikaela Duguil <kimmieduggz@gmail.com>
2021-04-06 20:59:28 -0700

    isAnagram

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,1 +1,6 @@
 /* exported isAnagram */
+function isAnagram(firstString, secondString) {
+  var firstStringSorted = firstString.split('').sort().join('').trim();
+  var secondStringSorted = secondString.split('').sort().join('').trim();
+  return firstStringSorted === secondStringSorted;
+}

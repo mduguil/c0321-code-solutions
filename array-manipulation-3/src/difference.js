1c5987b650d1d3e1d27e7b7e0bbdd421caca8398
Mikaela Duguil <kimmieduggz@gmail.com>
2021-04-07 12:22:41 -0700

    difference

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,15 @@
 /* exported difference */
+function difference(first, second) {
+  var uniqueArr = [];
+  first.forEach(function (item) {
+    if (!second.includes(item)) {
+      uniqueArr.push(item);
+    }
+  });
+  second.forEach(function (item) {
+    if (!first.includes(item)) {
+      uniqueArr.push(item);
+    }
+  });
+  return uniqueArr;
+}

5fe718af08870fd0c921d5889c647aa5d30dc6ff
Mikaela Duguil <kimmieduggz@gmail.com>
2021-04-07 11:59:29 -0700

    unique

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,10 @@
 /* exported unique */
+function unique(array) {
+  var uniqueArr = [];
+  array.forEach(function (item) {
+    if (!uniqueArr.includes(item)) {
+      uniqueArr.push(item);
+    }
+  });
+  return uniqueArr;
+}

2463f1ddaef1d080863f49b23905a3781f93e1f1
Mikaela Duguil <kimmieduggz@gmail.com>
2021-04-06 18:02:54 -0700

    omit

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,10 @@
 /* exported omit */
+function omit(source, keys) {
+  var omitObj = {};
+  Object.keys(source).forEach(function (item) {
+    if (!keys.includes(item)) {
+      omitObj[item] = source[item];
+    }
+  });
+  return omitObj;
+}
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,1 +1,10 @@
 /* exported pick */
+function pick(source, keys) {
+  var pickObj = {};
+  Object.keys(source).forEach(function (item) {
+    if (keys.includes(item)) {
+      pickObj[item] = source[item];
+    }
+  });
+  return pickObj;
+}

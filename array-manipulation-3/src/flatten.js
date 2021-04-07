8b46b7f1b8f9b76a82d7b82a7b13667f6c914b2d
Mikaela Duguil <kimmieduggz@gmail.com>
2021-04-07 11:27:50 -0700

    flatten

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,1 +1,14 @@
 /* exported flatten */
+function flatten(array) {
+  var flatArr = [];
+  array.forEach(function (item) {
+    if (Array.isArray(item)) {
+      item.forEach(function (i) {
+        flatArr.push(i);
+      });
+    } else {
+      flatArr.push(item);
+    }
+  });
+  return flatArr;
+}

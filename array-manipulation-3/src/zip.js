71e5146c10f985b997a7d9a1f649f4c83cbf498e
Mikaela Duguil <kimmieduggz@gmail.com>
2021-04-07 11:52:21 -0700

    zip

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,15 @@
 /* exported zip */
+function zip(first, second) {
+  var shortArr = first;
+  var combinedArr = [];
+  if (second.length < first.length) {
+    shortArr = second;
+  }
+  for (var i = 0; i < shortArr.length; i++) {
+    var miniArr = [];
+    miniArr.push(first[i]);
+    miniArr.push(second[i]);
+    combinedArr.push(miniArr);
+  }
+  return combinedArr;
+}

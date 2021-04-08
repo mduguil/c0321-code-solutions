05a65a2455756f52eda1b9b3ccfa5ade93c2e3c7
Mikaela Duguil <kimmieduggz@gmail.com>
2021-04-08 13:14:56 -0700

    dots switch

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -6,9 +6,13 @@ var slideNum = 1;
 function showNext(event) {
   var currentSlide = document.querySelector('.char' + slideNum);
   currentSlide.className = 'characters char' + slideNum + ' hidden';
+
+  var currentDot = document.querySelector('.dot' + slideNum);
+  currentDot.className = 'far fa-circle dot' + slideNum;
   slideNum++;
   if (slideNum > 5) slideNum = 1;
   showNextImg();
+  showNextDot();
 }
 
 function showNextImg(event) {
@@ -16,17 +20,30 @@ function showNextImg(event) {
   nextSlide.className = 'characters char' + slideNum;
 }
 
+function showNextDot(event) {
+  var nextDot = document.querySelector('.dot' + slideNum);
+  nextDot.className = 'fas fa-circle dot' + slideNum;
+}
+
 function showPrevious(event) {
   var currentSlide = document.querySelector('.char' + slideNum);
   currentSlide.className = 'characters char' + slideNum + ' hidden';
+  var currentDot = document.querySelector('.dot' + slideNum);
+  currentDot.className = 'far fa-circle dot' + slideNum;
   slideNum--;
   if (slideNum < 1) slideNum = 1;
   showPreviousImg();
+  showPreviousDot();
 }
 
 function showPreviousImg(event) {
   var nextSlide = document.querySelector('.char' + slideNum);
   nextSlide.className = 'characters char' + slideNum;
+}
+
+function showPreviousDot(event) {
+  var previousDot = document.querySelector('.dot' + slideNum);
+  previousDot.className = 'fas fa-circle dot' + slideNum;
 }
 
 $arrowLeft.addEventListener('click', showPrevious);

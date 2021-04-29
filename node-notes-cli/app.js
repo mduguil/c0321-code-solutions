15be96520c9be52964afb1e69d2d72ece79d6763
Mikaela Duguil <kimmieduggz@gmail.com>
2021-04-29 16:57:32 -0700

    can delete data

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const command = process.argv[2];
 
 const read = () => {
-  fs.readFile('./data.json', 'utf8', (err, data) => {
+  fs.readFile('data.json', 'utf8', (err, data) => {
     if (err) throw err;
 
     const dataObj = JSON.parse(data);
@@ -13,8 +13,59 @@ const read = () => {
   });
 };
 
+const create = () => {
+  const input = process.argv[3];
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+
+    const dataObj = JSON.parse(data);
+    let nextId = dataObj.nextId;
+
+    const combineData = data => {
+      dataObj.notes[nextId] = data;
+
+      nextId++;
+      dataObj.nextId = nextId;
+
+      return dataObj;
+    };
+
+    const combinedData = JSON.stringify(combineData(input), null, 2);
+
+    fs.writeFile('data.json', combinedData, 'utf8', err => {
+      if (err) throw err;
+    });
+  });
+};
+
+const deleteInput = () => {
+  const idNum = process.argv[3];
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+
+    const dataObj = JSON.parse(data);
+
+    const deleteData = id => {
+      delete dataObj.notes[id];
+      return dataObj;
+    };
+
+    const shortendData = JSON.stringify(deleteData(idNum), null, 2);
+
+    fs.writeFile('data.json', shortendData, 'utf8', err => {
+      if (err) throw err;
+    });
+  });
+};
+
 switch (command) {
   case 'read':
     read();
+    break;
+  case 'create':
+    create();
+    break;
+  case 'delete':
+    deleteInput();
     break;
 }

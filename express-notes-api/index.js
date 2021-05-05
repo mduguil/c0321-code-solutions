fe0414ce43b02dfbc3b6a5eee908dbd51f0b1022
Mikaela Duguil <kimmieduggz@gmail.com>
2021-05-04 18:44:55 -0700

    set up

---
+++ express-notes-api/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+app.get('/api/notes', (req, res) => {
+  const dataArr = [];
+
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    const notes = data.notes;
+    for (const key in notes) {
+      dataArr.push(notes[key]);
+    }
+    res.json(dataArr);
+    res.status(200);
+  });
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Connected to port 3000');
+});

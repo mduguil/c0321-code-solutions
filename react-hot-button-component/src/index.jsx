ba4ad209035eafca15111b90b62863332038b9c5
Mikaela Duguil <kimmieduggz@gmail.com>
2021-05-14 14:58:35 -0700

    react practice

--- react-hot-button-component/src/index.jsx
+++ react-hot-button-component/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CustomButton from './hot-button';
+import CustomButton from './hotButton';
 
 ReactDOM.render(
   <CustomButton />,

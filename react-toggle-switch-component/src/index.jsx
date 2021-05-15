041de7a55e9420d9ac1b1db23eade90cb173e641
Mikaela Duguil <kimmieduggz@gmail.com>
2021-05-14 17:15:57 -0700

    toggle button using react

---
+++ react-toggle-switch-component/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ToggleButton from './toggleSwitch';
+
+ReactDOM.render(
+  <ToggleButton />,
+  document.querySelector('#root')
+);

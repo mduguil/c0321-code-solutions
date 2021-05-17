72baad73644a9025769b78ea119e6654babaa6b4
Mikaela Duguil <kimmieduggz@gmail.com>
2021-05-17 16:06:17 -0700

    stopwatch functions

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -4,23 +4,55 @@ class Stopwatch extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isTicking: false
+      isTicking: false,
+      counter: 0
     };
+    this.tick = this.tick.bind(this);
+    this.reset = this.reset.bind(this);
     this.buttonHandler = this.buttonHandler.bind(this);
   }
 
   buttonHandler() {
     const { isTicking } = this.state;
+    if (!isTicking) {
+      this.setState({
+        isTicking: true
+      });
+      this.timerID = setInterval(this.tick, 1000);
+    } else {
+      this.setState({
+        isTicking: false
+      });
+      clearInterval(this.timerID);
+    }
+  }
 
+  tick() {
+    const { counter } = this.state;
     this.setState({
-      isTicking: !isTicking
+      counter: counter + 1
     });
   }
 
+  reset() {
+    if (!this.state.isTicking) {
+      this.setState({
+        counter: 0
+      });
+    }
+  }
+
   render() {
+    let icon = 'fas fa-play';
+    if (this.state.isTicking) {
+      icon = 'fas fa-pause';
+    }
     return (
       <div id="container">
-        <div className="stopwatch">0</div>
+        <div onClick={!this.state.isTicking ? this.reset : this.buttonHandler} className="stopwatch">
+            <span className="counter">{this.state.counter}</span>
+        </div>
+          <i onClick={this.buttonHandler} className={icon}></i>
       </div>
     );
   }

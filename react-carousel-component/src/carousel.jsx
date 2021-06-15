aaefa0c8b354d56bc2fb3f171f1eeb290fdca576
Mikaela Duguil <kimmieduggz@gmail.com>
2021-06-15 11:55:01 -0700

    carousel switches img every second

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -4,19 +4,33 @@ export default class Carousel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currSlide: 1
+      currSlide: 0
     };
   }
 
+  componentDidMount() {
+    setInterval(() => {
+      if (this.state.currSlide < this.props.pokemons.length - 1) {
+        this.setState({
+          currSlide: this.state.currSlide + 1
+        });
+      } else {
+        this.setState({
+          currSlide: 0
+        });
+      }
+    }, 1000);
+  }
+
   render() {
-    return this.props.pokemons.map((pokemon, i) => {
-      return (
-        <div className="container" key={i}>
-          <div className="pokemon-container">
-          <img src={pokemon} className="pokemon" />
-          </div>
+    const currSlide = this.state.currSlide;
+    const pokemons = this.props.pokemons;
+    return (
+      <div className="container">
+        <div className="pokemon-container">
+        <img src={pokemons[currSlide]} className="pokemon" />
         </div>
-      );
-    });
+      </div>
+    );
   }
 }
